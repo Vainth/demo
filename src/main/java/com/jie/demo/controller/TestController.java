df8e09a8a626c973ce17fd32db6a718a8e8efd01
YingJie Zhu <zhuyingjie1230@qq.com>
2023-03-25 09:17:56 +0800

    new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/jie/demo/DemoApplication.java 	new file:   src/main/java/com/jie/demo/controller/TestController.java 	new file:   src/main/resources/application.yaml 	new file:   src/test/java/com/jie/demo/DemoApplicationTests.java 	new file:   test.http

---
+++ src/main/java/com/jie/demo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.jie.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public String geString(){
+        return "测试成功";
+    }
+}
